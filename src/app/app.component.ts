a639302a87370d07fe408d188e18fd3e2f548768
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-16 15:49:47 -0300

    Exploring and writing → all file which had been generated → inline code in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  /*template: `<h1>Hello from inline template</h1>
+  <p>This template is written inline</p>
+  `,*/
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
