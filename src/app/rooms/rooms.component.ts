77e9f91693526bbac94a5900a142a6640b0c3d4e
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-17 14:38:23 -0300

    Applying some logic

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'hinv-rooms',
@@ -18,9 +19,33 @@ export class RoomsComponent implements OnInit {
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+    // alert("Clicked")
   }
 
   show() {
     this.hideTitle = !this.hideTitle;
+
+  }
+
+  addRoom() {
+    this.numberOfRooms = this.numberOfRooms + 1;
+
+    if (this.numberOfRooms === 25) {
+      alert("Fully available");
+      document.getElementById("add")?.setAttribute("disabled","");
+    }
+  }
+
+  takeRoom() {
+    this.numberOfRooms = this.numberOfRooms - 1;
+
+    if (this.numberOfRooms <= 0) {
+      this.numberOfRooms = 0;
+      alert("Fully booked");
+    }
+
+    if (this.numberOfRooms < 25) {
+      document.getElementById("add")?.removeAttribute('disabled');
+    }
   }
 }
