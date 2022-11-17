3f0c962b11242d7861f393ab8bed73cce4e32b95
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-17 10:13:09 -0300

    Adding component "rooms"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
   /*template: `<h1>Hello from inline template</h1>
   <p>This template is written inline</p>
   `,*/
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  //styles: ['h1 {color: red;}'] //can add styles through here as well.
 })
 export class AppComponent {
   title = 'Hotel_Inventory_App';
